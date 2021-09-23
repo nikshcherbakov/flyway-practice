9e462a1bbc57c405ba357e50c1470b52bcc3ed34
nikshcherbakov <shcerbackow@yandex.ru>
2021-09-23 06:43:44 +0300

    Initial commit

---
+++ src/test/java/com/nikshcherbakov/flywaypractice/FlywayPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nikshcherbakov.flywaypractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlywayPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
